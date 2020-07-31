44bd14b3598d23ed96b7edcd897e41882c14d05a
MartinGK <mrtgeka@gmail.com>
2020-07-31 16:30:51 -0300

    Singup dev

---
+++ client/src/components/auth/Signup.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default function Signup() {
+    return (
+        <form>
+            <fieldset>
+                <label>Email</label>
+            </fieldset>
+            <fieldset>
+                <label>Password</label>
+            </fieldset>
+        </form>
+    )
+}
